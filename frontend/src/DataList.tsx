1f0d01bfd1820e2da13959e21fab2a4536bbf934
Burak <sbsomuncuoglu@gmail.com>
2020-08-28 12:14:37 -0400

    working v0

--- frontend/src/DataList.tsx
+++ frontend/src/DataList.tsx
@@ -1,10 +1,10 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, Component } from "react";
 import { Query } from "react-apollo";
 import { GetAllQuery } from "./queries/GetAllQuery";
-import "./CharacterList.css";
+import "./DataList.css";
 
-interface Props {
-  
+interface DataState {
+ 
 }
 
 export interface Stats {
@@ -21,19 +21,60 @@ interface Data {
   getNews: News;
 }
 
-const DataList: React.FC<Props> = () => {
+const DataList = () => {
+
+  let displayData: JSX.Element[] = [];
+
+  let getInitialState = function() {
+    var initialDisplayData = localStorage.getItem( 'displayData' ) || [];
+
+    return initialDisplayData;
+  }
+
+  let persistData = function(_data: any) {
+      localStorage.setItem( 'displayData', _data );
+  }
+
   return (
-    <div className="CharacterList">
+    <div className="DataList">
       <h2>NY Covid Data</h2>
+
+      <button onClick={() => {
+         displayData.push(
+          <p>
+            <b>Total Cases:</b>
+            <br></br>
+            <b>Date:</b> 
+            <br></br>
+            <b>News:</b>
+          </p>);
+        }}>
+        Update
+      </button>
+
       <Query<Data> query={GetAllQuery}>
         {({ loading, error, data }) => {
-          if (loading) return "Loading..." as Element;
-          if (error || !data) return `Error!`;
+          if (loading) return (<div>Loading...</div>);
+          if (error || !data) return (<div>Error...</div>);
+
+          console.log(displayData);
+
+          {
+            displayData.push(
+              <p>
+                <b>Total Cases:</b> {data.getStats.total_cases  }
+                <br></br>
+                <b>Date:</b> {data.getStats.date  }
+                <br></br>
+                <b>News:</b> {data.getNews.notes  }
+              </p>
+            )
+          };
 
           return (
-            <ul>
-              
-            </ul>
+            <div id="display-data-container">
+              {displayData}
+            </div>
           );
         }}
       </Query>
